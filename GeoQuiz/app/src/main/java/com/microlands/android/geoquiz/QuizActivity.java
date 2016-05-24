930b2c394b1474df0172e7607e8b0fc590560661
Luis Vivero <luis.a.vivero@gmail.com>
2016-05-23 23:20:21 -0700

    Laying Out the User Interface

--- GeoQuiz/app/src/main/java/com/microlands/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/microlands/android/geoquiz/QuizActivity.java
@@ -2,12 +2,19 @@ package com.microlands.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
     }
 }
