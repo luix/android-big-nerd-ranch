e5bdcba189aaf426b21073eda403bfe5f159b812
Luis Vivero <luis.a.vivero@gmail.com>
2016-05-24 21:42:16 -0700

    Setting listeners

--- GeoQuiz/app/src/main/java/com/microlands/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/microlands/android/geoquiz/QuizActivity.java
@@ -2,7 +2,9 @@ package com.microlands.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -15,6 +17,24 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Todo: show a toast message
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
