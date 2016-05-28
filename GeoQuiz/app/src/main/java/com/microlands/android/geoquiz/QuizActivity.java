6064730e521868a5015e3519079a8fe1fd3a62da
Luis Vivero <luis.a.vivero@gmail.com>
2016-05-27 22:52:51 -0700

    Challenges: added a listener to the TextView, a previous button and changed Buttons to ImageButtons.

--- GeoQuiz/app/src/main/java/com/microlands/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/microlands/android/geoquiz/QuizActivity.java
@@ -22,6 +22,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,7 +32,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -51,6 +53,12 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                advanceQuestion();
+            }
+        });
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -68,15 +76,37 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
+                advanceQuestion();
             }
         });
 
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                previousQuestion();
+            }
+        });
+
+        updateQuestion();
+    }
+
+    private void previousQuestion() {
+        helper(-1);
+    }
+
+    private void advanceQuestion() {
+        helper(1);
+    }
+
+    private void helper(int i) {
+        int foo = mCurrentIndex + i;
+        int bar = (foo < 0) ? mQuestionBank.length - 1 : foo;
+        mCurrentIndex = bar % mQuestionBank.length;
         updateQuestion();
     }
 
