a815373c6a9048d3bb4f30b339ffd22fb2326213
Luis Vivero <luis.a.vivero@gmail.com>
2016-06-27 23:25:33 -0700

    Setting up project for BeatBox - Assets.

---
+++ BeatBox/app/src/main/java/com/microlands/android/beatbox/BeatBoxActivity.java
@@ -0,0 +1,13 @@
+package com.microlands.android.beatbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_beat_box);
+    }
+}
---
+++ BeatBox/app/src/main/java/com/microlands/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,44 @@
+package com.microlands.android.beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by luisvivero on 6/27/16.
+ */
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_beat_box, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view
+                .findViewById(R.id.fragment_beat_box_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return view;
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private Button mButton;
+
+        public SoundHolder(LayoutInflater inflater, ViewGroup container) {
+            super(inflater.inflate(R.layout.list_item_sound, container, false));
+
+            mButton = (Button) itemView.findViewById(R.id.list_item_sound_button);
+        }
+    }
+
+}
