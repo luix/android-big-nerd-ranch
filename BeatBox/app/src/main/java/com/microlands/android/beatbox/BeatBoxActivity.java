e5d6469b6b8b52b765b3098b2358903469672b8d
Luis Vivero <luis.a.vivero@gmail.com>
2016-06-28 23:05:51 -0700

    Adding Assets.

--- BeatBox/app/src/main/java/com/microlands/android/beatbox/BeatBoxActivity.java
+++ BeatBox/app/src/main/java/com/microlands/android/beatbox/BeatBoxActivity.java
@@ -1,13 +1,13 @@
 package com.microlands.android.beatbox;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_beat_box);
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
     }
 }
---
+++ BeatBox/app/src/main/java/com/microlands/android/beatbox/Sound.java
@@ -0,0 +1,25 @@
+package com.microlands.android.beatbox;
+
+/**
+ * Created by luisvivero on 6/28/16.
+ */
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
