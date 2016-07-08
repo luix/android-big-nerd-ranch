122917c0f0c5f8aae51e0aea54e62ead9c950285
Luis Vivero <luis.a.vivero@gmail.com>
2016-07-07 23:19:29 -0700

    Using WebChromeClient and ProgressBar.

--- PhotoGallery/app/src/main/java/com/microlands/android/photogallery/PhotoPageFragment.java
+++ PhotoGallery/app/src/main/java/com/microlands/android/photogallery/PhotoPageFragment.java
@@ -4,11 +4,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by luisvivero on 7/7/16.
@@ -20,6 +23,7 @@ public class PhotoPageFragment extends Fragment {
 
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static PhotoPageFragment newInstance(Uri uri) {
         Bundle args = new Bundle();
@@ -35,8 +39,28 @@ public class PhotoPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_page, container, false);
 
+        mProgressBar = (ProgressBar) view.findViewById(R.id.fragment_photo_page_progress_bar);
+        mProgressBar.setMax(100); // WebChromeClient reports in range of 0 - 100
+
         mWebView = (WebView) view.findViewById(R.id.fragment_photo_page_web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
