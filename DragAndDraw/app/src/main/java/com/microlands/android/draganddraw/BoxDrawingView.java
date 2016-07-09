7e4b3634f4424050d65449cf70b38c5848669507
Luis Vivero <luis.a.vivero@gmail.com>
2016-07-08 21:13:20 -0700

    Handling Touch Events.

--- DragAndDraw/app/src/main/java/com/microlands/android/draganddraw/BoxDrawingView.java
+++ DragAndDraw/app/src/main/java/com/microlands/android/draganddraw/BoxDrawingView.java
@@ -1,7 +1,10 @@
 package com.microlands.android.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -9,6 +12,7 @@ import android.view.View;
  */
 
 public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
 
     public BoxDrawingView(Context context) {
         this(context, null);
@@ -16,5 +20,34 @@ public class BoxDrawingView extends View {
 
     public BoxDrawingView(Context context, AttributeSet attributes) {
         super(context, attributes);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+
+        Log.i(TAG, action + " at x = " + current.x + " , y = " + current.y);
+
+        return true;
     }
 }
