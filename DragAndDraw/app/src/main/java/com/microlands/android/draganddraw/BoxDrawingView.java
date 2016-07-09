549c5ffae18754d5300aa9f9b1e5d09fd48035fb
Luis Vivero <luis.a.vivero@gmail.com>
2016-07-08 20:48:51 -0700

    Creating a Custom View.

---
+++ DragAndDraw/app/src/main/java/com/microlands/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,20 @@
+package com.microlands.android.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by luisvivero on 7/8/16.
+ */
+
+public class BoxDrawingView extends View {
+
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    public BoxDrawingView(Context context, AttributeSet attributes) {
+        super(context, attributes);
+    }
+}
