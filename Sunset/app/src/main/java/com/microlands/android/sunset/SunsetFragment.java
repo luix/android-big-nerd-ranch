9835ff3f45d9d5ecfccca7cea106efc5da81fdf1
Luis Vivero <luis.a.vivero@gmail.com>
2016-07-09 20:45:14 -0700

    Simple Property Animation.

--- Sunset/app/src/main/java/com/microlands/android/sunset/SunsetFragment.java
+++ Sunset/app/src/main/java/com/microlands/android/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.microlands.android.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,10 @@ import android.view.ViewGroup;
 
 public class SunsetFragment extends Fragment {
 
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
     }
@@ -21,6 +26,29 @@ public class SunsetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
         return view;
+    }
+
+    private void startAnimation() {
+        float sunYstart = mSunView.getTop();
+        float sunYend = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYstart, sunYend)
+                .setDuration(3000);
+
+        heightAnimator.start();
     }
 }
